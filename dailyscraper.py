6403587f86f78cdb208b6537a8a6afb9379690c7
Jamal Elmir <81425259+blankprogram@users.noreply.github.com>
2024-01-03 00:43:07 +1100

    changes to scraper

--- dailyscraper.py
+++ dailyscraper.py
@@ -8,16 +8,17 @@ import time
 
 def get_daily_leetcode_question():
     url = 'https://leetcode.com/problemset/'
-    chrome_bin = os.environ['GOOGLE_CHROME_BIN']
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = chrome_bin
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
     
 
     try:
-        driver = webdriver.chrome(executable_path= os.environ['CHROMEDRIVER_PATH'], options=chrome_options)
+        driver = webdriver.chrome(executable_path= CHROMEDRIVER_PATH, options=chrome_options)
         driver.get(url)
         time.sleep(5) 
 
