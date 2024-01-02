4d70266e1871dd8fa2b3d54a8f03392b70349b34
Jamal Elmir <81425259+blankprogram@users.noreply.github.com>
2024-01-03 01:40:58 +1100

    web driver

--- dailyscraper.py
+++ dailyscraper.py
@@ -1,26 +1,24 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 def get_daily_leetcode_question():
     url = 'https://leetcode.com/problemset/'
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     
 
     try:
-        service = Service(CHROMEDRIVER_PATH)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        service = Service(str(os.environ.get('CHROMEDRIVER_PATH')))
+        driver = webdriver.Chrome(service = service, options=chrome_options)
         driver.get(url)
         time.sleep(5) 
 
