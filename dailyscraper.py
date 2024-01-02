fb1d230bbf6880e3e27c481ac158a496b6f5701b
Jamal Elmir <81425259+blankprogram@users.noreply.github.com>
2024-01-03 02:11:41 +1100

    fixes for selenium running on heroku

--- dailyscraper.py
+++ dailyscraper.py
@@ -1,7 +1,7 @@
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
@@ -11,11 +11,14 @@ def get_daily_leetcode_question():
     url = 'https://leetcode.com/problemset/'
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    
 
     try:
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        service = Service(str(os.environ.get('CHROMEDRIVER_PATH')))
+        driver = webdriver.Chrome(service = service, options=chrome_options)
         driver.get(url)
         time.sleep(5) 
 
@@ -24,7 +27,8 @@ def get_daily_leetcode_question():
         print(f"An error occurred: {e}")
         return None
     finally:
-        driver.quit()
+        if 'driver' in locals():
+            driver.close()
 
     soup = BeautifulSoup(page_source, 'html.parser')
     tz = pytz.timezone('UTC')
